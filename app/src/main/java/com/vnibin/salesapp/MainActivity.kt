2cb1d3b5cb000a4c0a5e62d4ec701cc074762d4b
vnibin <vnibin.93@gmail.com>
2018-05-30 12:11:25 +0530

    Signup design

--- app/src/main/java/com/vnibin/salesapp/MainActivity.kt
+++ app/src/main/java/com/vnibin/salesapp/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.vnibin.salesapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        login_signup.setOnClickListener {
+
+            var i=Intent(this,RegAct::class.java)
+            startActivity(i)
+        }
     }
 }
