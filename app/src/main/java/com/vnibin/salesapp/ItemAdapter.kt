1877e55f91e95d3c002bf49d3c61d9a2085836d5
vnibin <vnibin.93@gmail.com>
2018-06-01 14:15:42 +0530

    dialog fragment

--- app/src/main/java/com/vnibin/salesapp/ItemAdapter.kt
+++ app/src/main/java/com/vnibin/salesapp/ItemAdapter.kt
@@ -1,5 +1,6 @@
 package com.vnibin.salesapp
 
+import android.app.Activity
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -14,7 +15,7 @@ import kotlinx.android.synthetic.main.items_row.view.*
 class ItemAdapter(var context:Context,var list:ArrayList<Items>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
-        (holder as ItemHolder).bind(list[position].name,list[position].price,list[position].photo)
+        (holder as ItemHolder).bind(list[position].name,list[position].price,list[position].photo,list[position].id)
     }
 
     override fun getItemCount(): Int {
@@ -29,13 +30,22 @@ class ItemAdapter(var context:Context,var list:ArrayList<Items>) : RecyclerView.
 class ItemHolder(itemview:View):RecyclerView.ViewHolder(itemview)
 {
 
-    fun bind(n:String,p:Double,u:String)
+    fun bind(n:String,p:Double,u:String,item_id:Int)
     {
         itemView.item_name.text=n;
         itemView.item_price.text=p.toString()
         var web:String="http://192.168.1.4//Companyweb/salesweb/images/"+u
        // web=web.replace("","%20")
         Picasso.with(itemView.context).load(web).into(itemView.item_photo)
+
+        itemView.item_add_photo.setOnClickListener {
+
+           UserInfo.itemId=item_id
+            var obj=QtyFragment()
+            var manager=(itemView.context as Activity).fragmentManager   // (itemView.context as Activity) this is used to specify fragmentmanager's context as activity as fragment manager is used in activity,, here it is used in adapter class which is not an activity
+             obj.show(manager,"Qty")
+
+        }
     }
 }
 
--- app/src/main/java/com/vnibin/salesapp/UserInfo.kt
+++ app/src/main/java/com/vnibin/salesapp/UserInfo.kt
@@ -8,5 +8,7 @@ class UserInfo
 {
     companion object {
         var mobile:String=""
+        var itemId:Int=0
+        var qty:Int=0
     }
 }
