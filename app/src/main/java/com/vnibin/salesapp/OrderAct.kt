0ea112fb01a4b0fa10a3dca25992c70d6ab4041c
vnibin <vnibin.93@gmail.com>
2018-06-01 16:46:47 +0530

    Cancel order

--- app/src/main/java/com/vnibin/salesapp/OrderAct.kt
+++ app/src/main/java/com/vnibin/salesapp/OrderAct.kt
@@ -11,9 +11,11 @@ import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.activity_order.*
+import kotlinx.android.synthetic.main.fragment_qty.*
 
 class OrderAct : AppCompatActivity() {
 
@@ -57,6 +59,28 @@ class OrderAct : AppCompatActivity() {
         {
             var i=Intent(this,HomeAct::class.java)
             startActivity(i)
+        }
+
+        if(item?.itemId==R.id.item_cancel)
+        {
+
+
+
+            var url="http://192.168.1.4/Companyweb/salesweb/cancel_order.php?mobile="+UserInfo.mobile
+
+            var rq:RequestQueue=Volley.newRequestQueue(this)
+            var sr= StringRequest(Request.Method.GET,url,Response.Listener { response ->
+
+                var i=Intent(this,HomeAct::class.java)
+                startActivity(i)
+
+            },Response.ErrorListener { error ->
+
+                Toast.makeText(this,error.message,Toast.LENGTH_SHORT).show()
+            })
+
+            rq.add(sr)
+
         }
         return super.onOptionsItemSelected(item)
     }
