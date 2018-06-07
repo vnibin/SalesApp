e2a6c8e31860f043d7848c8d33d8f0f258c013c2
vnibin <vnibin.93@gmail.com>
2018-06-07 17:08:27 +0530

    Paypal integration

--- app/src/main/java/com/vnibin/salesapp/UserInfo.kt
+++ app/src/main/java/com/vnibin/salesapp/UserInfo.kt
@@ -10,5 +10,6 @@ class UserInfo
         var mobile:String=""
         var itemId:Int=0
         var qty:Int=0
+        var client_id:String="Ac9xbI0eetRnb-iqhlfqmUzMFPJBQhvCpPvM4zT5XUqP3df4Gpbc0t62u3ZusV-2sdwW6hq4NL0qqy4j"
     }
 }
